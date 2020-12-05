a4f37c9633238815ef21a32b41d08de2b1c06816
storm.breaker <67917417+hillarymandich@users.noreply.github.com>
2020-12-05 15:40:27 -0500

    testing

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
 #Dependencies
+#typical python modules 
+import datetime as dt
 import numpy as np
+import pandas as pd
+#sqlalchemy modules
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
+#flask module 
 from flask import Flask, jsonify
-import datetime as dt
 
-#create setup info
+
+#Setup SQLAlchemy 
 #create engine
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
-## reflect an existing database/tables
+##reflect an existing database/tables
 Base = automap_base()
 Base.prepare(engine, reflect=True)
 
@@ -22,15 +27,16 @@ station = Base.classes.station
 # Create our session (link) from Python to the DB
 session = Session(engine)
 
-#declare flask app
+#flast setup - declare flask app
 app = Flask(__name__)
 
-#flask routes
+#Build routes
 #List all routes that are available
 @app.route("/")
 def home():
     return (
-        f"Routes List:<br/>"
+        f"Step 2 - Climate App<br/>"
+        f"Available Routes:<br/>"
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
@@ -38,8 +44,17 @@ def home():
         f"/api/v1.0/<start>/<end>"
     )
 
-
-
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    
+    prcp_data = session.query(measurement.date).all()
+    
+#create dictionary
+    results_dict = {}
+    for result in prcp_data:
+        results_dict[result[0]] = result[1]
+    # Jsonify summary
+    return jsonify(results_dict)
 
 
 #if main run 
